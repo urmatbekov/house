24ea1b11cb594a28d92aa579836c8d0f160df420
Ваше Имя <you@example.com>
2020-10-22 20:07:34 +0600

    underline

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,21 +6,44 @@ class Header extends Component {
         return (
             <header>
                 <nav className="navbar">
-                    <ul className="nav-items">
-                        <li>О нас</li>
-                        <li>Объекты</li>
-                        <li>Коммерция</li>
-                        <li>Новости</li>
-                        <li>Клиентам</li>
-                        <li>Оплата</li>
-                        <li>Контакты</li>
-                    </ul>
-                    <ul className="nav-settings">
-                        <li>0 (555) 555-555</li>
-                    </ul>
-                    <div className="logo">
-                        <img alt="Logo Elite House"
-                             src="https://static.tildacdn.com/tild3638-3239-4339-a436-636666313131/elite-house-logo-whi.png"/>
+                    <div className="container">
+                        <ul className="nav-items">
+                            <li>
+                                <a href="#">О нас</a>
+                                <span className="line"/>
+                            </li>
+                            <li>
+                                <a href="#">Объекты</a>
+                                <span className="line"/>
+                            </li>
+                            <li>
+                                <a href="#">Коммерция</a>
+                                <span className="line"/>
+                            </li>
+                            <li>
+                                <a href="#">Новости</a>
+                                <span className="line"/>
+                            </li>
+                            <li>
+                                <a href="#">Клиентам</a>
+                                <span className="line"/>
+                            </li>
+                            <li><a href="#">Оплата</a>
+                                <span className="line"/>
+                            </li>
+                            <li><a href="#">Контакты</a>
+                                <span className="line"/>
+                            </li>
+                        </ul>
+                        <ul className="nav-settings">
+                            <li><a href="#">0 (555) 555-555</a></li>
+                        </ul>
+                        <div className="logo">
+                            <a>
+                                <img alt="Logo Elite House"
+                                     src="https://static.tildacdn.com/tild3638-3239-4339-a436-636666313131/elite-house-logo-whi.png"/>
+                            </a>
+                        </div>
                     </div>
                 </nav>
             </header>
