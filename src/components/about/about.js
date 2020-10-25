c340044e3d9ef61eca7f14d3d3e0d17f7c1fd205
Your Name <you@example.com>
2020-10-26 01:10:28 +0600

    news add

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -4,9 +4,9 @@ import "./about.css";
 class About extends Component {
     render() {
         return (
-            <section className="container">
-                <h2 className="th-elem">О нас </h2>
-                <div className="th-atom">
+            <section className="container my-container">
+                <h2 className="th-elem ">О нас </h2>
+                <div className="th-atom float-left">
                     Компания Elite House основана в 2013 году<br/>
                     и является одним из лидеров строительной <br/>
                     отрасли страны. Сегодня Elite House<br/>
@@ -16,7 +16,7 @@ class About extends Component {
                     современными инновациями.
                 </div>
                 <div>
-                    <img src="https://static.tildacdn.com/tild6265-6630-4638-a434-343938353661/1.jpg"/>
+                    <img className="th-atom-img1 float-right" src="https://static.tildacdn.com/tild6265-6630-4638-a434-343938353661/1.jpg"/>
                 </div>
             </section>
         );
--- src/components/atom/atom.js
+++ src/components/atom/atom.js
@@ -6,7 +6,7 @@ class Atom extends Component {
     render() {
         return (
             <div>
-                <div className='my-atom d-flex justify-content-around'>
+                <div className=' container my-atom d-flex justify-content-between"'>
                     <div className='th-atom'>
                         <img
                             className='th-atom-img'
@@ -19,14 +19,14 @@ class Atom extends Component {
                         <img
                             className='th-atom-img'
                             src='https://static.tildacdn.com/tild3234-3061-4935-b939-333264343438/Frame_4.svg'/>
-                        <p className="th-text d-inline">1</p>
+                        <p className="th-text d-inline">№1</p>
                         <p className="th-text1 d-inline">ВЫБОР ГОДА 2019</p>
                     </div>
                     <div className='th-atom'>
                         <img
                             className='th-atom-img'
                             src='https://static.tildacdn.com/tild3661-6166-4133-b632-353033343638/Frame_3.svg'/>
-                        <p className="th-text d-inline">№7</p>
+                        <p className="th-text d-inline">7</p>
                         <p className="th-text1 d-inline">ЛЕТ НА РЫНКЕ</p>
                     </div>
                 </div>
