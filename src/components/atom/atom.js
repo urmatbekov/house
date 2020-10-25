01ff1fe0325ac4f79f42d89201c833de744adf3d
Your Name <you@example.com>
2020-10-26 01:47:34 +0600

    atom and new style

--- src/components/atom/atom.js
+++ src/components/atom/atom.js
@@ -6,28 +6,28 @@ class Atom extends Component {
     render() {
         return (
             <div>
-                <div className=' container my-atom d-flex justify-content-between"'>
+                <div className='container my-atom d-flex justify-content-between'>
                     <div className='th-atom'>
                         <img
                             className='th-atom-img'
                             src='https://static.tildacdn.com/tild6134-3436-4561-b238-663537343431/Frame_2.svg'/>
-                        <p className="th-text d-inline">1700</p>
-                        <p className="th-text1 d-inline">И БОЛЕЕ ДОЛЬЩИКОВ</p>
+                        <p className="th-text ">1700</p>
+                        <p className="th-text1 ">И БОЛЕЕ ДОЛЬЩИКОВ</p>
 
                     </div>
-                    <div className='th-atom'>
+                    <div className='th-atom th-elem1'>
                         <img
                             className='th-atom-img'
                             src='https://static.tildacdn.com/tild3234-3061-4935-b939-333264343438/Frame_4.svg'/>
-                        <p className="th-text d-inline">№1</p>
-                        <p className="th-text1 d-inline">ВЫБОР ГОДА 2019</p>
+                        <p className="th-text ">№1</p>
+                        <p className="th-text1 ">ВЫБОР ГОДА 2019</p>
                     </div>
-                    <div className='th-atom'>
+                    <div className='th-atom th-elem2'>
                         <img
                             className='th-atom-img'
                             src='https://static.tildacdn.com/tild3661-6166-4133-b632-353033343638/Frame_3.svg'/>
-                        <p className="th-text d-inline">7</p>
-                        <p className="th-text1 d-inline">ЛЕТ НА РЫНКЕ</p>
+                        <p className="th-text">7</p>
+                        <p className="th-text1">ЛЕТ НА РЫНКЕ</p>
                     </div>
                 </div>
             </div>
--- src/components/news/news.js
+++ src/components/news/news.js
@@ -4,9 +4,9 @@ import "./news.css"
 class News extends Component {
      render() {
         return (
-            <div className='container d-inline-flexB B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B                                          M BCCCC justify-content-between news-container row'>
+            <div className='container d-inline-flex justify-content-between news-container row'>
                 <h2>Новости</h2>
-                <div className="card col">
+                <div className="card">
                     <img className="card-img-top" src="https://static.tildacdn.com/tild6661-3137-4630-a635-383063663464/7.jpg"/>
                         <div className="card-body">
                             <p className="card-text">Тимур Файзиев готов инвестировать в бизнес-идею.</p>
@@ -14,14 +14,14 @@ class News extends Component {
                             <p className='card-text'>Строительная компания Elite House представляет социальный проект «Бизнес Өкүл Ата», который направлен на развитие предпринимательства и поддержку новых бизнес-проектов в Кыргызской Республике.</p>
                         </div>
                 </div>
-                <div className="card col">
+                <div className="card ">
                     <img className="card-img-top" src="https://static.tildacdn.com/tild3464-6237-4964-b064-323366613461/new.png"/>
                         <div className="card-body">
                             <p className="card-text">Elite House разыгрывает квартиру!</p>
                             <p className="card-text">Строительная компания Elite House запускает грандиозную акцию! Приобретая любую недвижимость, вы получаете один лотерейный билет, который будет участвовать в розыгрыше 1-комнатной квартиры в жилом комплексе "Эл Классик Плюс". И это еще не всё! Новый iPhone-11 ждет каждого покупателя в период акции!</p>
                         </div>
                 </div>
-                <div className="card col">
+                <div className="card">
                     <img className="card-img-top" src="https://static.tildacdn.com/tild3966-6562-4631-a632-336530656666/51.jpg"/>
                         <div className="card-body">
                             <p className="card-text">
