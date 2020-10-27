d1df1f89cc30aeb446a1af588e04879b90222c07
mr-oroz <oroz7307@gmail.com>
2020-10-27 20:13:41 +0600

    contackt

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -19,9 +19,8 @@ class App extends Component {
                 <News/>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d97055.11567120106!2d72.7379563068642!3d40.52010190615417!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38bdac017f5a2e85%3A0xe0a3c61dc6f47b95!2z0J7RiA!5e0!3m2!1sky!2skg!4v1603807191354!5m2!1sky!2skg"
-                    width="600" height="450" frameBorder="0" style={{border:0}} allowFullScreen="" aria-hidden="false"
+                    width="600" height="450" frameBorder="0" style={{border: 0}} allowFullScreen="" aria-hidden="false"
                     tabIndex="0"/>
->>>>>>> df10d5e13cc998e0e0aca3428820e84cab599be3
             </div>
         );
     }
