df10d5e13cc998e0e0aca3428820e84cab599be3
Ваше Имя <you@example.com>
2020-10-27 20:07:20 +0600

    kontakt

---
+++ src/components/news/news-item/news-item.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class NewsItem extends Component {
+    render() {
+        return (
+            <div className="news-container">
+                <a><img className='img-news'
+                        src='https://static.tildacdn.com/tild6661-3137-4630-a635-383063663464/7.jpg'/></a>
+                <p className="th-item">
+                    <strong>Тимур Файзиев готов инвестировать в бизнес-идею</strong>
+                </p>
+                <p className="th-item">
+                    <strong>до 100 000 долларов и стать наставником!</strong>
+                </p>
+                <div className="t778_des">Строительная компания Elite House представляет социальный проект
+                    «Бизнес
+                    Өкүл Ата», который направлен на развитие предпринимательства и поддержку новых
+                    бизнес-проектов в
+                    Кыргызской Республике.
+                </div>
+            </div>
+        );
+    }
+}
+
+export default NewsItem;
